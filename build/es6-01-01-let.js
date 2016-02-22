5dc2c3fb934fb61e2913e2efcc5618ec97fb8f29
guoyff <guoyff@yonyou.com>
2016-02-22 21:13:48 +0800

    初始化项目配置

---
+++ build/es6-01-01-let.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var a = 'hello world';
+
+var PI = 3.148;
+
+// const PI = 2.17;
+
+console.log(a);
+
+console.log(PI);
---
+++ src/es6-01-01-let.js
@@ -0,0 +1,10 @@
+
+let a = 'hello world';
+
+const PI = 3.148;
+
+// const PI = 2.17;
+
+console.log( a );
+
+console.log( PI );
